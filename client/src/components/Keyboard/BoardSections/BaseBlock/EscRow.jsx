350ffbe599bc201c27dc4d8794cb9b56bf3e5f48
karlnh <hubigkarl@gmail.com>
2023-05-03 22:50:29 -0500

    Create elements for escape key row

---
+++ client/src/components/Keyboard/BoardSections/BaseBlock/EscRow.jsx
@@ -0,0 +1,28 @@
+export default function EscRow() {
+    return (
+        <div id="esc-row" className="key-row">
+            <span className="pure-button-group">
+                <button className="pure-button">esc</button>
+            </span>
+            <span className="pure-button-group">
+                <button className="pure-button">F1</button>
+                <button className="pure-button">F2</button>
+                <button className="pure-button">F3</button>
+                <button className="pure-button">F4</button>
+            </span>
+            <span className="pure-button-group">
+                <button className="pure-button">F5</button>
+                <button className="pure-button">F6</button>
+                <button className="pure-button">F7</button>
+                <button className="pure-button">F8</button>
+            </span>
+            <span className="pure-button-group">
+                <button className="pure-button">F9</button>
+                <button className="pure-button">F10</button>
+                <button className="pure-button">F11</button>
+                <button className="pure-button">F12</button>
+            </span>
+        </div>
+    )
+
+}
