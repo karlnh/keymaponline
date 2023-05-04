eea3c99ba7baa021660d5c11919dc28c881324d2
karlnh <hubigkarl@gmail.com>
2023-05-03 22:50:48 -0500

    Create elements for tab key row

---
+++ client/src/components/Keyboard/BoardSections/BaseBlock/TabRow.jsx
@@ -0,0 +1,22 @@
+export default function TabRow() {
+    return (
+        <div id="tab-row" className="key-row">
+            <span className="pure-button-group">
+                <button className="pure-button">tab</button>
+                <button className="pure-button">q</button>
+                <button className="pure-button">w</button>
+                <button className="pure-button">e</button>
+                <button className="pure-button">r</button>
+                <button className="pure-button">t</button>
+                <button className="pure-button">y</button>
+                <button className="pure-button">u</button>
+                <button className="pure-button">i</button>
+                <button className="pure-button">o</button>
+                <button className="pure-button">p</button>
+                <button className="pure-button">[</button>
+                <button className="pure-button">]</button>
+                <button className="pure-button">\</button>
+            </span>
+        </div>
+    )
+}
