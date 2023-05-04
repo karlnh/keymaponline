e6383a4956dce4b5e58cd71f7b3a823c827a5b39
karlnh <hubigkarl@gmail.com>
2023-05-03 22:50:08 -0500

    Create elements for capslock key row

---
+++ client/src/components/Keyboard/BoardSections/BaseBlock/CapsRow.jsx
@@ -0,0 +1,21 @@
+export default function CapsRow() {
+    return (
+        <div id="caps-row">
+            <span className="pure-button-group">
+                <button className="pure-button">caps lock</button>
+                <button className="pure-button">a</button>
+                <button className="pure-button">s</button>
+                <button className="pure-button">d</button>
+                <button className="pure-button">f</button>
+                <button className="pure-button">g</button>
+                <button className="pure-button">h</button>
+                <button className="pure-button">j</button>
+                <button className="pure-button">k</button>
+                <button className="pure-button">l</button>
+                <button className="pure-button">;</button>
+                <button className="pure-button">&apos;</button>
+                <button className="pure-button">Enter</button>
+            </span>
+        </div>
+    )
+}
