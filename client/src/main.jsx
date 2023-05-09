0ee8ffe77d154b0536e8396c0406a19ee555ca2c
karlnh <hubigkarl@gmail.com>
2023-05-09 16:46:44 -0500

    Create test for grabbing all buttons

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App.jsx'
 import './styles/index.css'
 
+const buttons = document.getElementsByTagName("button");
+console.log(buttons);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
